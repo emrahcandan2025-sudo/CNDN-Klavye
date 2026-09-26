313b9489b215f6886108305911c95e7ee296be76
emrahcandan2025-sudo <emrahcandan2025@gmail.com>
2026-09-26 08:16:47 +0300

    Update MainActivity.kt

--- app/src/main/java/com/cndn/ime/MainActivity.kt
+++ app/src/main/java/com/cndn/ime/MainActivity.kt
@@ -1,22 +1,23 @@
 package com.cndn.ime
 
+import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
 import android.provider.Settings
 import android.view.inputmethod.InputMethodManager
-import androidx.appcompat.app.AppCompatActivity
+import android.widget.Button
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : Activity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        findViewById<android.widget.Button>(R.id.btnEnable).setOnClickListener {
+        findViewById<Button>(R.id.btnEnable).setOnClickListener {
             startActivity(Intent(Settings.ACTION_INPUT_METHOD_SETTINGS))
         }
 
-        findViewById<android.widget.Button>(R.id.btnSwitch).setOnClickListener {
+        findViewById<Button>(R.id.btnSwitch).setOnClickListener {
             val imm = getSystemService(INPUT_METHOD_SERVICE) as InputMethodManager
             imm.showInputMethodPicker()
         }
