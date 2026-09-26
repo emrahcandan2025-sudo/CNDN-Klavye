316528d31041fde51a606c72461bef2d6b727f8c
emrahcandan2025-sudo <emrahcandan2025@gmail.com>
2026-09-26 08:05:41 +0300

    Create Dictionary.kt

---
+++ app/src/main/java/com/cndn/ime/Dictionary.kt
@@ -0,0 +1,18 @@
+package com.cndn.ime
+
+object Dictionary {
+    val words: List<String> = listOf(
+        "merhaba", "nasılsın", "iyiyim", "teşekkürler", "evet", "hayır",
+        "tamam", "kaç", "saat", "bugün", "yarın", "dün", "lütfen", "affedersin",
+        "görüşürüz", "selam", "naber", "güzel", "iyi", "kötü", "hızlı", "yavaş",
+        "ev", "iş", "okul", "araba", "telefon", "bilgisayar", "internet",
+        "arkadaş", "aile", "anne", "baba", "kardeş", "sevgili", "aşk",
+        "para", "zaman", "hayat", "dünya", "gün", "gece", "sabah", "akşam",
+        "kahve", "çay", "su", "yemek", "kitap", "müzik", "film", "oyun",
+        "gel", "git", "gelecek", "gitti", "yapıyorum", "yapacağım", "istiyorum",
+        "seviyorum", "biliyorum", "bilmiyorum", "anladım", "anlamadım",
+        "nerede", "neden", "nasıl", "kim", "ne", "hangi", "kaça", "kaçta",
+        "buyur", "rica", "özür", "dilerim", "hoşça", "kal", "kalın", "sağlık",
+        "mesaj", "yazı", "cevap", "soru", "cevapla", "bekle", "acil", "önemli"
+    )
+}
